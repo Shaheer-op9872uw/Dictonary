587289cd679a788584a8d9a4cf2f60473a75f8f6
Shaheer-op9872uw <shaheerminhas123@gmail.com>
2024-11-02 20:42:10 +0500

    Dictnaory

---
+++ act2.py
@@ -0,0 +1,10 @@
+test_dict = {'codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'coding' : 1}
+print("the oring dic :" + str(test_dict))
+
+K = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+
+print("FREQUENCY OF K IS : " + str(res))
---
+++ act3.py
@@ -0,0 +1,8 @@
+SPCMcountry_code = {'india' : '0091',
+                    'australia' :'0025',
+                    'nepal : '0000'}
+print ("country code for india -")
+print(country_code.get(india', 'not found))
+
+print("country code for japan-")
+print(country_code.get('japan', 'not found'))
